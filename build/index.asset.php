8609ce0eaddb59ed5655c6dd716cd554fa9f03eb
Omar ElHawary <omaraelhawary@gmail.com>
2024-08-02 23:52:24 +0300

    Change search JS to use Vanilla JS

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => 'ca315ba3fec32f32feee');
+<?php return array('dependencies' => array(), 'version' => 'afee4830e6693bbb44d2');
--- src/modules/Search.js
+++ src/modules/Search.js
@@ -1,102 +1,105 @@
-import $ from 'jquery';
-
 class Search {
     //1. describe our object
     constructor() {
         this.addSearchHtml();
-        this.resultDiv = $("#search-overlay__results");
-        this.openButton = $('.js-search-trigger');
-        this.closeButton = $('.search-overlay__close');
-        this.searchOverlay = $('.search-overlay');
-        this.searchField = $('#search-term');
+        this.resultDiv = document.getElementById("search-overlay__results");
+        this.openButton = document.querySelectorAll('.js-search-trigger');
+        this.closeButton = document.querySelector('.search-overlay__close');
+        this.searchOverlay = document.querySelector('.search-overlay');
+        this.searchField = document.getElementById('search-term');
         this.events();
         this.isOverlayOpen = false;
         this.isSpinnerVisible = false;
-        this.preValue;
+        this.preValue = "";
         this.typingTimer;
     }
+
     //2. all events
     events() {
-        this.openButton.on('click', this.openOverlay.bind(this));
-        this.closeButton.on('click', this.closeOverlay.bind(this));
-        $(document).on('keydown', this.keyPressDispatcher.bind(this));
-        this.searchField.on('keyup', this.typingLogic.bind(this));
+        this.openButton.forEach(button => button.addEventListener('click', this.openOverlay.bind(this)));
+        this.closeButton.addEventListener('click', this.closeOverlay.bind(this));
+        document.addEventListener('keydown', this.keyPressDispatcher.bind(this));
+        this.searchField.addEventListener('keyup', this.typingLogic.bind(this));
     }
+
     //3. methods functions or methods
     openOverlay() {
-        this.searchOverlay.addClass("search-overlay--active");
-        $("body").addClass('body-no-scroll');
+        this.searchOverlay.classList.add("search-overlay--active");
+        document.body.classList.add('body-no-scroll');
         setTimeout(() => {
             this.searchField.focus();
         }, 301);
         this.isOverlayOpen = true;
-        this.searchField.val('');
+        this.searchField.value = '';
     }
+
     closeOverlay() {
-        this.searchOverlay.removeClass('search-overlay--active');
-        $("body").removeClass('body-no-scroll');
+        this.searchOverlay.classList.remove('search-overlay--active');
+        document.body.classList.remove('body-no-scroll');
         this.isOverlayOpen = false;
-        this.resultDiv.html('');
+        this.resultDiv.innerHTML = '';
     }
+
     keyPressDispatcher(e) {
-        if (e.keyCode == 83 && !this.isOverlayOpen && !$("input, textarea").is(':focus')) {
+        if (e.keyCode === 83 && !this.isOverlayOpen && !document.querySelector("input:focus, textarea:focus")) {
             this.openOverlay();
         }
-        if (e.keyCode == 27 && this.isOverlayOpen) {
+        if (e.keyCode === 27 && this.isOverlayOpen) {
             this.closeOverlay();
         }
     }
+
     typingLogic() {
-        if (this.searchField.val() != this.preValue) {
+        if (this.searchField.value !== this.preValue) {
             clearTimeout(this.typingTimer);
-            if (this.searchField.val()) {
+            if (this.searchField.value) {
                 if (!this.isSpinnerVisible) {
-                    this.resultDiv.html('<div class="spinner-loader"></div>');
+                    this.resultDiv.innerHTML = '<div class="spinner-loader"></div>';
                     this.isSpinnerVisible = true;
                 }
                 this.typingTimer = setTimeout(this.getResults.bind(this), 750);
             } else {
-                this.resultDiv.html('');
+                this.resultDiv.innerHTML = '';
                 this.isSpinnerVisible = false;
             }
-
         }
-        this.preValue = this.searchField.val();
+        this.preValue = this.searchField.value;
     }
+
     getResults() {
-        $.when(
-            $.getJSON(uniData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val()),
-            $.getJSON(uniData.root_url + '/wp-json/wp/v2/pages?search=' + this.searchField.val())
-        ).then((posts, pages) => {
-            var combineResults = posts[0].concat(pages[0]);
-            this.resultDiv.html(`
+        Promise.all([
+            fetch(uniData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.value).then(res => res.json()),
+            fetch(uniData.root_url + '/wp-json/wp/v2/pages?search=' + this.searchField.value).then(res => res.json())
+        ]).then(([posts, pages]) => {
+            const combineResults = posts.concat(pages);
+            this.resultDiv.innerHTML = `
                 <h2 class="search-overlay__section-title">Search Results</h2>
                 ${combineResults.length ? '<ul class="link-list min-list">' : '<p>No results</p>'}
                     ${combineResults.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('')}
                 ${combineResults.length ? '</ul>' : ''}
-            `);
+            `;
             this.isSpinnerVisible = false;
-        },
-            () => {
-                this.resultDiv.html("<p>No results</p>");
-            });
+        }).catch(() => {
+            this.resultDiv.innerHTML = "<p>No results</p>";
+        });
     }
+
     addSearchHtml() {
-        $('body').append(`
+        document.body.insertAdjacentHTML('beforeend', `
             <div class="search-overlay">
-            <div class="search-overlay__top">
+                <div class="search-overlay__top">
+                    <div class="container">
+                        <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+                        <input class="search-term" id="search-term" type="text" placeholder="What are you looking for?">
+                        <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+                    </div>
+                </div>
                 <div class="container">
-                    <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
-                    <input class="search-term" id="search-term" type="text" placeholder="What are you looking for?">
-                    <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+                    <div id="search-overlay__results"></div>
                 </div>
             </div>
-            <div class="container">
-                <div id="search-overlay__results"></div>
-            </div>
-            </div>
-    `)
+        `);
     }
 }
 
-export default Search
+export default Search;
