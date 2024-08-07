47521cce1f6ea3159790d89f211903d93f3f4627
Omar ElHawary <omaraelhawary@gmail.com>
2024-08-07 20:37:03 +0300

    Creating API Endpoint for proffessors and customize data

--- inc/search-route.php
+++ inc/search-route.php
@@ -10,5 +10,21 @@ function unvierstyRegisterSeach(){
 add_action('rest_api_init', 'unvierstyRegisterSeach');
 
 function universitySearchResults(){
-    return "welcome";
+    $proffessors = new WP_Query(array(
+        'post_type' => 'professor',
+    ));
+
+    $proffessorsResult = array();
+    
+    while($proffessors -> have_posts()){
+        $proffessors -> the_post();
+        array_push($proffessorsResult, array(
+            'id' => get_the_id(),
+            'title' => get_the_title(),
+            'link' => get_the_permalink(),
+            'professorImage' => get_the_post_thumbnail_url(0, 'professorLandscape'),
+        ));
+    }
+
+    return $proffessorsResult;
 }
