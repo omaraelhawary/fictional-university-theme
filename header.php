adf32afa7aa1595ca136284d2865c1f758bd8eb5
Omar ElHawary <omaraelhawary@gmail.com>
2024-08-23 23:31:29 +0300

    Use navtive WP functions for Login and registration buttons

--- header.php
+++ header.php
@@ -43,10 +43,11 @@
                     </a>
                     <?php
                         } else { ?>
-                    <a href=" <?php echo esc_url(site_url('/wp-admin.php')); ?>"
+                    <a href=" <?php echo wp_login_url(); ?>"
                         class="btn btn--small btn--orange float-left push-right">Login</a>
-                    <a href="<?php echo esc_url(site_url('/wp-signup.php')); ?>"
-                        class="btn btn--small btn--dark-orange float-left">Sign Up</a>
+                    <a href="<?php echo wp_registration_url() ?>"
+                        class="btn btn--small btn--dark-orange float-left">Sign
+                        Up</a>
                     <?php
                         }
                     ?>
